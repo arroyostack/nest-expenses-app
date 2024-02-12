507720b24ecd9743224a92f539cf4fa068eae409
arroyostack <arroyostack@gmail.com>
2024-02-12 20:17:15 +0100

    Feat: App controllers routes.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Delete, Get, Post, Put } from "@nestjs/common";
 
-@Controller()
+// Controllers are in charge of handling incoming requests and returning responses to the client.
+@Controller( 'report/:reportType' )
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAllReports() {
+    return [{ "report": "This will return all income report" }];
+  }
+
+  @Get( '/:id' )
+  getReportById() {
+    return { "report": "This will return income report by id" };
+  }
+
+  @Post()
+  createReport() {
+    return { "report": "This will create a new income report" };
+  }
+
+  @Put( '/:id' )
+  updateReport() {
+    return { "report": "This will update income report by id" };
+  }
+
+  @Delete( '/:id' )
+  deleteReport() {
+    return { "report": "This will delete all income report" };
   }
 }
